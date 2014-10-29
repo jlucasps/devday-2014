8f549e1395b23c2bee80d3468fade202ec8cb767
Joo Lucas <jlucasps@gmail.com>
2014-10-29 20:38:19 +0000

    Add external JS

---
+++ webapp/assets/effects.js
@@ -0,0 +1,6 @@
+var section = document.getElementsByTagName('section')[0],
+    span = document.createElement('span');
+
+span.innerText = "#crp #wpo #perfmatters #devday2014";
+span.style.fontWeight = "bold";
+section.appendChild( span );
